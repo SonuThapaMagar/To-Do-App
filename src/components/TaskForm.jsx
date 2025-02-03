eb7ed9843c0dffa951a8d8e5a0bb01252f893faa
SonuThapaMagar <sonumagar787@gmail.com>
2025-02-03 14:53:36 +0545

    Header css adjusted

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -2,10 +2,9 @@ import React from 'react'
 
 const TaskForm = () => {
     return (
-        <header className='mx-auto text-center p-4 mt-12'>
-            <form action="">
-                <input type="text" className='' placeholder='Enter your task' />
-
+        <header className='flex justify-center items-center mx-auto text-center py-6 mt-12'>
+            <form action="" className='w-full max-w-3xl space-y-6 px-4 rounded'>
+                <input type="text" className='w-100 text-xl bg-gray-200 border-rounded p-4 mt-4 py-2' placeholder='Enter your task' />
                 <div>
                     <button>HTML</button>
                     <button>CSS</button>
