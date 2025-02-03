7529c6a53aa57ae93dd35a48078eb8c4dd8c484f
SonuThapaMagar <sonumagar787@gmail.com>
2025-02-03 15:28:09 +0545

    Header section done

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -4,19 +4,22 @@ const TaskForm = () => {
     return (
         <header className='flex justify-center items-center mx-auto text-center py-6 mt-12'>
             <form action="" className='w-full max-w-3xl space-y-6 px-4 rounded'>
-                <input type="text" className='w-100 text-xl bg-gray-200 border-rounded p-4 mt-4 py-2' placeholder='Enter your task' />
-                <div>
-                    <button>HTML</button>
-                    <button>CSS</button>
-                    <button>JavaScript</button>
-                    <button>React</button>
-
-                    <select name="" id="" className="task_status">
-                        <option value="todo">To Do</option>
-                        <option value="doing">Doing</option>
-                        <option value="done">Done</option>
-                    </select>
-                    <button type='submit' className='task_submit'>Add Task</button>
+                <input type="text" className='w-100 text-xl bg-gray-200 border-rounded rounded p-4 mt-4 py-2' placeholder='Enter your task' />
+                <div className='flex items-center justify-between'>
+                    <div>
+                        <button className='font-sm bg-gray-200 cursor-pointer mx-2 py-2 px-10 rounded'>HTML</button>
+                        <button className='font-sm bg-gray-200 cursor-pointer mx-2 py-2 px-10 rounded'>CSS</button>
+                        <button className='font-sm bg-gray-200 cursor-pointer mx-2 py-2 px-10 rounded'>JavaScript</button>
+                        <button className='font-sm bg-gray-200 cursor-pointer mx-2 py-2 px-10 rounded'>React</button>
+                    </div>
+                    <div>
+                        <select name="" id="" className="font-sm bg-gray-200 cursor-pointer mx-2 py-2  rounded">
+                            <option value="todo">To Do</option>
+                            <option value="doing">Doing</option>
+                            <option value="done">Done</option>
+                        </select>
+                        <button type='submit' className='text-sm bg-purple-600 text-white  mx-2 py-2 cursor-pointer rounded px-2'>Add Task</button>
+                    </div>
                 </div>
             </form>
         </header>
