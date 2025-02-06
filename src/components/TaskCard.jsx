54d777d3a17dc3f0cc0e4af6849b1fed645797f6
SonuThapaMagar <sonumagar787@gmail.com>
2025-02-06 14:19:28 +0545

    Styles added in TaskCard

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -2,21 +2,22 @@ import React from 'react';
 import Tag from "./Tag";
 import { FaTrash } from "react-icons/fa";
 
-const TaskCard = ({ title, tags, index, deleteTask}) => {
+const TaskCard = ({ title, tags, index, deleteTask }) => {
     console.log("Rendering TaskCard with:", { title, tags });
 
     return (
-        <article className="text-left p-4 bg-gray-100 rounded">
-            <p className="mb-2">{title}</p>
+        <article className="text-left p-4 rounded-lg max-w-xs sm:max-w-md mx-auto mb-4">
+            <p className="mb-2 text-base sm:text-lg font-semibold">{title}</p>
 
-            <div className="flex items-center justify-between">
-                <div className='flex gap-2'>
-                    {tags.map((tag, index) => (
-                        <Tag key={index} tagName={tag} selected />
-                    ))}
-                </div>
-                 <FaTrash
-                    className="text-rose-800 cursor-pointer"
+            <div className="flex flex-wrap gap-2 mb-2">
+                {tags.map((tag, index) => (
+                    <Tag key={index} tagName={tag} selected />
+                ))}
+            </div>
+
+            <div className="flex justify-between items-center">
+                <FaTrash
+                    className="text-rose-800 cursor-pointer text-xl sm:text-2xl hover:text-rose-600 transition-all"
                     title="Delete"
                     onClick={() => deleteTask({ title, tags })}
                 />
